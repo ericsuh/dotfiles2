63445167814e03244a1e67e85e34fed05cbda041
Eric J. Suh <esuh@descript.com>
2024-10-29 11:13:19 -0700

    Another fix

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,8 +1,8 @@
 set fish_greeting
 if status is-interactive
     if test $TERM_PROGRAM = "vscode"
-	    set -xg EDITOR=code
+	    set -xg EDITOR code
 	else
-	    set -xg EDITOR=nvim
+	    set -xg EDITOR nvim
 	end
 end
