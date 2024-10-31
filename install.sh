12f843915df16f05d8d6e40b12427c0c2bde197f
Eric J. Suh <esuh@descript.com>
2024-10-31 14:19:37 -0700

    Have install script force apply

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if ! command -v chezmoi; then
 fi
 
 if [ -d ~/.local/share/chezmoi ]; then
-    chezmoi update --apply --keep-going
+    chezmoi update --apply --keep-going --force
 else
-    chezmoi init --apply ericsuh/dotfiles2
+    chezmoi init --apply --force ericsuh/dotfiles2
 fi
