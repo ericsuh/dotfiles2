4e5c63aec1f5587c28819a2584690274f8db490c
Eric J. Suh <esuh@descript.com>
2025-04-02 00:06:07 -0700

    Remove output noise

--- dot_config/fish/conf.d/envs.fish
+++ dot_config/fish/conf.d/envs.fish
@@ -1,9 +1,9 @@
 set --export --global SHELL (command --search fish)
 
 if status --is-interactive
-    if command --search nvim
+    if command --search nvim >/dev/null
         set --export --global EDITOR (command --search nvim)
-    else if command --search vim
+    else if command --search vim >/dev/null
         set --export --global EDITOR (command --search vim)
     end
 end
