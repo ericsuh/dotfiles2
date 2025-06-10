5936179861b3164066c7f2ab7cd1ce1f8f7a3a0c
Eric Suh <eric@ericsuh.com>
2025-06-09 22:33:36 -0700

    Upgrade config for nvim-treesitter plugin

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -4,53 +4,45 @@ return {
     branch = "main",
     lazy = false,
     config = function () 
-        local configs = require("nvim-treesitter.configs")
-
-        configs.setup({
-            ensure_installed = {
-                "bash",
-                "c",
-                "css",
-                "dockerfile",
-                "gitignore",
-                "go",
-                "gotmpl",
-                "html",
-                "javascript",
-                "json",
-                "jsonc",
-                "lua",
-                "make",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "rust",
-                "sql",
-                "starlark",
-                "terraform",
-                "toml",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "yaml",
-                "zig",
-            },
-            sync_install = false,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true },
-            autotag = {
-                enable = true,
-            },
-            rainbow = {
-                enable = true,
-                extended_mode = false,
-                max_file_lines = nil,
-            },
+        local treesitter = require("nvim-treesitter")
+        treesitter.setup()
+        local langs = {
+            "bash",
+            "c",
+            "css",
+            "dockerfile",
+            "gitignore",
+            "go",
+            "gotmpl",
+            "html",
+            "javascript",
+            "json",
+            "jsonc",
+            "lua",
+            "make",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "query",
+            "rust",
+            "sql",
+            "starlark",
+            "terraform",
+            "toml",
+            "tsx",
+            "typescript",
+            "vim",
+            "vimdoc",
+            "yaml",
+            "zig",
+        }
+        treesitter.install(langs)
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = langs,
+            callback = function()
+                vim.treesitter.start()
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
         })
     end
 }
