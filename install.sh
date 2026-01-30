b800da297b03fd0d194ee928022cb510194b33da
Eric J. Suh <ericsuh@users.noreply.github.com>
2026-01-30 09:43:09 -0800

    Update install script to use mise for chezmoi commands

--- install.sh
+++ install.sh
@@ -2,17 +2,30 @@
 
 set -euo pipefail
 
-CMD=chezmoi
+mkdir -p "$HOME/.local/bin"
+mkdir -p "$HOME/.config/mise"
+
+export PATH="$HOME/.local/bin${PATH:+:${PATH}}"
+
+if ! command -v curl >/dev/null; then
+    sudo apt-get install --yes --no-install-recommends curl
+fi
+
+curl -fsSL -o "$HOME/.local/mise/config.toml" https://raw.githubusercontent.com/ericsuh/dotfiles2/refs/heads/main/dot_config/mise/config.toml
+
+if ! command -v mise >/dev/null; then
+    curl -fsSL https://mise.run | sh
+fi
+mise install
+
 if ! command -v chezmoi >/dev/null; then
     pushd ~/.local >/dev/null
     sh -c "$(curl -fsLS get.chezmoi.io)"
-    CMD=~/.local/bin/chezmoi
     popd
 fi
 
 if [ -d ~/.local/share/chezmoi ]; then
-    "$CMD" update --apply --keep-going --force
+    mise exec -- chezmoi update --apply --keep-going --force
 else
-    "$CMD" init --apply --force ericsuh/dotfiles2
+    mise exec -- chezmoi init --apply --force ericsuh/dotfiles2
 fi
-
